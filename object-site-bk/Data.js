79d3355697d07f6aeed9f1bab10f59745607c5cd
noProblem <StiLinius2255@yandex.ru>
2025-10-26 21:52:04 +0300

    new data

--- object-site-bk/Data.js
+++ object-site-bk/Data.js
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 export const User = [
   {
     id: 1,
@@ -12,8 +15,15 @@ export const Track = [
     id: 1,
     name: "Track 1",
     author: "NoNmae",
-    image: null,
-    url: "track1.mp3 ",
+    image: `${process.env.BASE_URL}/media/retro-design-2.jpg`,
+    url: `${process.env.BASE_URL}/media/track-1.mp3`,
+  },
+  {
+    id: 2,
+    name: "Track 2",
+    author: "NoNmae 2",
+    image: `${process.env.BASE_URL}/media/retro-design-3.jpg`,
+    url: `${process.env.BASE_URL}/media/track-2.mp3`,
   },
 ];
 
@@ -21,6 +31,10 @@ export const UserTrack = [
   {
     idUser: 1,
     idTrack: 1,
+  },
+  {
+    idUser: 1,
+    idTrack: 2,
   },
 ];
 
