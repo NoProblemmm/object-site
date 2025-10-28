cd9ebfa020594d97ad16c4f09e6822b851066ec6
noProblem <StiLinius2255@yandex.ru>
2025-10-28 16:20:17 +0300

    change authorization logic

--- object-site-bk/src/service/authorization.js
+++ object-site-bk/src/service/authorization.js
@@ -1,25 +1,22 @@
-import { User } from "./Data.js";
-import { generateTokens } from "./src/token/Token.js";
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-dotenv.config();
+import { User } from "../../Data.js";
+import { generateTokens } from "../common/token/Token.js";
 
-const JWT_SECRET = process.env.JWT_SECRET;
 export function authorizeUser(email, password) {
   const user = User.find((u) => u.email === email);
 
   if (!user) {
-    return { success: false, message: "Пользователь не найден" };
+    return { success: false, message: "User not found!" };
   }
 
   if (user.password !== password) {
-    return { success: false, message: "Неверный пароль" };
+    return { success: false, message: "Invalid password!" };
   }
 
   const token = generateTokens({ id: user.id, email: user.email });
   const accessToken = token.accessToken;
   const refreshToken = token.refreshToken;
   return {
+    user,
     success: true,
     accessToken,
     refreshToken,
