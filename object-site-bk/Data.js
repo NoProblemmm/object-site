3cb43435ee2347c3f9145aca83116fdea7c06937
noProblem <StiLinius2255@yandex.ru>
2025-10-28 16:19:27 +0300

    add data

--- object-site-bk/Data.js
+++ object-site-bk/Data.js
@@ -15,6 +15,7 @@ export const Track = [
     id: 1,
     name: "Track 1",
     author: "NoNmae",
+    time: "02:49",
     image: `${process.env.BASE_URL}/media/retro-design-2.jpg`,
     url: `${process.env.BASE_URL}/media/track-1.mp3`,
   },
@@ -22,8 +23,25 @@ export const Track = [
     id: 2,
     name: "Track 2",
     author: "NoNmae 2",
+    time: "02:06",
     image: `${process.env.BASE_URL}/media/retro-design-3.jpg`,
     url: `${process.env.BASE_URL}/media/track-2.mp3`,
+  },
+  {
+    id: 3,
+    name: "Track 3",
+    author: "NoName 3",
+    time: "03:07",
+    image: `${process.env.BASE_URL}/media/track-3.jpg`,
+    url: `${process.env.BASE_URL}/media/track-3.mp3`,
+  },
+  {
+    id: 4,
+    name: "Track 4",
+    author: "NoName 4",
+    time: "02:49",
+    image: `${process.env.BASE_URL}/media/track-4.jpg`,
+    url: `${process.env.BASE_URL}/media/track-4.mp3`,
   },
 ];
 
