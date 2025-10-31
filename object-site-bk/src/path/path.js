2942acfdcf030b0281605e1dbbd30f70386cb9be
noProblem <StiLinius2255@yandex.ru>
2025-10-31 20:37:43 +0300

    add path logic

--- object-site-bk/src/path/path.js
+++ object-site-bk/src/path/path.js
@@ -4,6 +4,7 @@ import { generateTokens } from "../common/token/Token.js";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
+
 // Обработка Логина
 export const login = (req, res) => {
   const { email, password } = req.body;
@@ -76,4 +77,46 @@ export const myTrack = (req, res) => {
     })
     .filter(Boolean);
   res.json(tracksForUser);
+};
+
+// Добавление в избранное объекта mp3
+export const addMyTrack = (req, res) => {
+  const userId = req.user.userId.id;
+  const trackId = Number(req.body.trackId);
+
+  const existingEntry = UserTrack.some(
+    (entry) => entry.idUser === userId && entry.idTrack === trackId
+  );
+
+  if (existingEntry) {
+    return res
+      .status(400)
+      .json({ message: "Этот трек уже добавлен в избранное." });
+  }
+
+  UserTrack.push({ idUser: userId, idTrack: trackId });
+
+  return res
+    .status(201)
+    .json({ message: "Трек успешно добавлен в избранное." });
+};
+
+// Удаление из избранного объекта mp3
+export const deleteMyTrack = (req, res) => {
+  const userId = req.user.userId.id;
+  const trackId = Number(req.body.trackId);
+
+  const indexToRemove = UserTrack.findIndex(
+    (entry) => entry.idUser === userId && entry.idTrack === trackId
+  );
+
+  if (indexToRemove !== -1) {
+    UserTrack.splice(indexToRemove, 1);
+
+    return res
+      .status(200)
+      .json({ message: "Трек успешно удалён из избранного." });
+  } else {
+    return res.status(404).json({ message: "Трек не найден в избранном." });
+  }
 };
