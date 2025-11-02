35d3d7dcea81896d7500edfbc460ea4efe594e9c
noProblem <StiLinius2255@yandex.ru>
2025-11-02 16:13:12 +0300

    refactoring

--- object-site-fr/src/components/home/playerLayout/layout/Layout.tsx
+++ object-site-fr/src/components/home/playerLayout/layout/Layout.tsx
@@ -1,14 +1,31 @@
 import { observer } from "mobx-react-lite";
 import { Player } from "../../../ui/home/player/Player";
+import { TabMenu } from "../../../ui/home/tabMenu/TabMenu";
+import { useSessionStore } from "../../../../store/session/Session.store";
+import { MyTrackMenu } from "../../../ui/home/myTrackMenu/MyTrackMenu";
+import { playerStore } from "../../../../store/player/Player.store";
 import "./Layout.css";
+import { useEffect } from "react";
 
 export const Layout = observer(() => {
+  const handleResize = () => {
+    if (window.innerWidth > 1000) {
+      playerStore.menuPage("NextTrack");
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+  }, []);
   return (
     <>
       <div className="container">
-        <div className="card__container">
-          <Player />
-        </div>
+        {useSessionStore.isAutentificate && <TabMenu />}
+        {playerStore.submenu === "MyTrack" && <MyTrackMenu />}
+        {playerStore.submenu === "NextTrack" && (
+          <div className="card__container">
+            <Player />
+          </div>
+        )}
       </div>
     </>
   );
