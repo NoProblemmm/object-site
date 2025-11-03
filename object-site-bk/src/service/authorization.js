542e08076feb2a8b4f332c20393b19a478004742
noProblem <StiLinius2255@yandex.ru>
2025-11-03 18:31:05 +0300

    add validation Signup

--- object-site-bk/src/service/authorization.js
+++ object-site-bk/src/service/authorization.js
@@ -1,15 +1,23 @@
 import { User } from "../../Data.js";
 import { generateTokens } from "../common/token/Token.js";
+import bcrypt from "bcrypt";
+import {
+  isValidEmail,
+  isValidName,
+  isValidPassword,
+} from "../common/validation/validation.js";
 
-export function authorizeUser(email, password) {
+export async function authorizeUser(email, password) {
   const user = User.find((u) => u.email === email);
-
   if (!user) {
     return { success: false, message: "User not found!" };
   }
+  const hashedPassword = await bcrypt.compare(password, user.password);
 
-  if (user.password !== password) {
-    return { success: false, message: "Invalid password!" };
+  if (!hashedPassword) {
+    if (password !== user.password) {
+      return { success: false, message: "Invalid password!" };
+    }
   }
 
   const token = generateTokens({ id: user.id, email: user.email });
@@ -21,4 +29,45 @@ export function authorizeUser(email, password) {
     accessToken,
     refreshToken,
   };
+}
+
+export function validationSignUp(email, password, name) {
+  try {
+    const user = User.find((u) => u.email === email);
+
+    if (!isValidEmail(email)) {
+      return {
+        success: false,
+        message: "Invalid email address!",
+      };
+    }
+    if (!isValidPassword(password)) {
+      return {
+        success: false,
+        message: "Invalid password!",
+      };
+    }
+    if (!isValidName(name)) {
+      return {
+        success: false,
+        message: "Invalid name!",
+      };
+    }
+
+    if (user) {
+      return {
+        success: false,
+        message: "A user with such an email already exists!",
+      };
+    } else {
+      return {
+        success: true,
+      };
+    }
+  } catch (error) {
+    return {
+      success: false,
+      message: "Internal server error",
+    };
+  }
 }
