015c078b14d9922d9bf6c4b0c4a86be21f68bb14
noProblem <StiLinius2255@yandex.ru>
2025-11-05 20:59:16 +0300

    add item tab-menu

--- object-site-fr/src/components/ui/home/tabMenu/TabMenu.tsx
+++ object-site-fr/src/components/ui/home/tabMenu/TabMenu.tsx
@@ -1,27 +1,39 @@
-import React from "react";
 import { playerStore } from "@store/player/Player.store";
 import "./TabMenu.css";
+import { observer } from "mobx-react-lite";
 
-export const TabMenu = () => {
+export const TabMenu = observer(() => {
   const handleMenu = (item: string) => {
     playerStore.menuPage(item);
   };
   return (
     <div className="tabmenu__container">
       <ul className="tabmenu__menu">
+        {playerStore.isMyTrack && (
+          <li
+            className="item__wind"
+            onClick={() =>
+              playerStore.playTrack(
+                Math.floor(Math.random() * playerStore.tracks.length)
+              )
+            }
+          >
+            Wind
+          </li>
+        )}
         <li
           className="tabmenu__menu-items"
-          onClick={() => handleMenu("NextTrack")}
+          onClick={() => handleMenu("Player")}
         >
-          NextTrack
+          Player
         </li>
         <li
           className="tabmenu__menu-items"
           onClick={() => handleMenu("MyTrack")}
         >
-          MyTrack
+          My track
         </li>
       </ul>
     </div>
   );
-};
+});
