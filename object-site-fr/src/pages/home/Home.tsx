2736b7cfd70e22ecb97374793787af26d773064c
noProblem <StiLinius2255@yandex.ru>
2025-11-13 20:32:41 +0300

    refactoring

--- object-site-fr/src/pages/home/Home.tsx
+++ object-site-fr/src/pages/home/Home.tsx
@@ -1,11 +1,9 @@
-import { useEffect } from "react";
 import { observer } from "mobx-react-lite";
 import { Layout } from "@components/home/playerLayout/layout/Layout";
 import { Footer } from "@components/ui/footer/Footer";
 import { Header } from "@components/ui/header/Head";
 import { LeftSider } from "@components/home/leftSider/LeftSider";
 import { RightSider } from "@components/home/rightSider/RightSider";
-import { playerStore } from "@store/player/Player.store";
 import { useSessionStore } from "@store/session/Session.store";
 import "./Home.css";
 
@@ -24,23 +22,7 @@ export const Home: React.FC = observer(() => {
       },
     },
   ];
-  useEffect(() => {
-    try {
-      useSessionStore
-        .refreshSession()
-        .then(() => {
-          if (!playerStore.tracks || playerStore.tracks.length === 0)
-            playerStore.getTrackStore();
-          if (!playerStore.myTracks || playerStore.myTracks.length === 0)
-            playerStore.getMyTrack();
-        })
-        .catch((error) => {
-          console.error("Error update session:", error);
-        });
-    } catch (err) {
-      console.error("Service error:", err);
-    }
-  }, []);
+
   return (
     <>
       <Header links={links} />
--- object-site-fr/src/routes/index.tsx
+++ object-site-fr/src/routes/index.tsx
@@ -1,6 +1,16 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Home } from "../pages/home/Home";
+import { useSessionStore } from "@store/session/Session.store";
+import { playerStore } from "@store/player/Player.store";
 
 export const Route = createFileRoute("/")({
+  beforeLoad: async () => {
+    useSessionStore.refreshSession().then(() => {
+      if (!playerStore.tracks || playerStore.tracks.length === 0)
+        playerStore.getTrackStore();
+      if (!playerStore.myTracks || playerStore.myTracks.length === 0)
+        playerStore.getMyTrack();
+    });
+  },
   component: Home,
 });
