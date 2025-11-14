8833d178a6779948e6f62dac84c2741a9cbaa949
noProblem <StiLinius2255@yandex.ru>
2025-11-14 19:10:30 +0300

    refactoring

--- object-site-fr/src/components/profile/ProfileLayout.tsx
+++ object-site-fr/src/components/profile/ProfileLayout.tsx
@@ -3,13 +3,41 @@ import { useProfileStore } from "@store/profile/Profile.store";
 import { TrackList } from "@components/ui/trackList/TrackList";
 import { playerStore } from "@store/player/Player.store";
 import { observer } from "mobx-react-lite";
+import { Button } from "@components/ui/button/Button";
 import "./ProfileLayout.css";
+import { useRef } from "react";
 
 export const ProfileLayout = observer(() => {
+  const avatarRef = useRef<HTMLInputElement | null>(null);
+
+  const handleAvatarChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const formData = new FormData();
+      formData.append("image", file);
+      try {
+        await useProfileStore.setImageUser(formData);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  const handleCompose = async () => {
+    await useProfileStore.requestCompose().then(async () => {
+      await useProfileStore.getProfile();
+    });
+  };
   return (
     <>
       <div className="profile__container">
         <div className="profile__left">
+          <input
+            hidden
+            type="file"
+            ref={avatarRef}
+            onChange={handleAvatarChange}
+          />
           <img
             src={
               useProfileStore.user?.image
@@ -18,8 +46,25 @@ export const ProfileLayout = observer(() => {
             }
             className="profile__img"
             alt="User img"
+            onClick={() => avatarRef.current!.click()}
           />
-          <span className="profile__name">{useProfileStore.user?.name}</span>
+          <span className="profile__name">
+            {useProfileStore.user?.admin && (
+              <span className="profile__prefix">[ADM] </span>
+            )}
+            {useProfileStore.user?.name}
+          </span>
+          <div className="profile__settings-container">
+            {useProfileStore.user?.isAuthor ? (
+              <Button className="profile__settings-btn">Upload music</Button>
+            ) : (
+              <Button className="profile__settings-btn" onClick={handleCompose}>
+                {useProfileStore.user?.requestComposer
+                  ? "Сancel the request "
+                  : "Become a composer"}
+              </Button>
+            )}
+          </div>
         </div>
         <div className="profile__right">
           <span className="profile__span-mytrack">My Track</span>
