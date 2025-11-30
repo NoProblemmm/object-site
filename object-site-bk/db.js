47b5bd1352ca39d3b0a50039059cfc6a6a31196e
noProblem <StiLinius2255@yandex.ru>
2025-11-30 23:26:29 +0300

    refactoring

--- object-site-bk/db.js
+++ object-site-bk/db.js
@@ -14,15 +14,9 @@ export const db = {
   query: async function (text, params) {
     const client = await pool.connect();
     try {
-      const startTime = Date.now();
       const res = await client.query(text, params);
-      const endTime = Date.now();
-
-      console.log(`Query execution time: ${endTime - startTime}ms.`);
-      console.log("Query result:", JSON.stringify(res));
       return res.rows;
     } catch (err) {
-      console.error("Database Error:", err.stack || err.message);
       throw err;
     } finally {
       client.release();
