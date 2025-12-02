02db865ea32ef073c8ac84bb3c76c9c84d0bc303
noProblem <StiLinius2255@yandex.ru>
2025-12-02 21:01:23 +0300

    hook useLocalStorage

---
+++ object-site-fr/src/hooks/useLocalStorage.ts
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+export type SetterOrUpdater<T> = ((value: T) => T) | T;
+
+export function useLocalStorage<T>(
+  key: string,
+  initialValue: T
+): [T, (value: T) => void] {
+  const stored = localStorage.getItem(key);
+  const initial = stored ? JSON.parse(stored) : initialValue;
+
+  const [state, setState] = useState<T>(initial);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      try {
+        localStorage.setItem(key, JSON.stringify(state));
+      } catch (err) {
+        console.error("Error localStorage:", err);
+      }
+    }
+  }, [key, state]);
+
+  return [
+    state,
+    (newValue: SetterOrUpdater<T>) => {
+      if (typeof newValue === "function") {
+        setState(newValue);
+      } else {
+        setState(newValue);
+      }
+    },
+  ];
+}
